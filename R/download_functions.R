e73f4fff3aca0f766b7e8498b0a53539deb65f37
Ethan <ethan.meyers@gmail.com>
2020-08-18 16:41:22 -0400

    Put all the download functions in a single file. Addded documentations and tests.

---
+++ R/download_functions.R
@@ -0,0 +1,151 @@
+
+
+#' Download a homework R Markdown file
+#'
+#' This function downloads homework files from the class GitHub repository.
+#'
+#' @param class_number A number indicating which homework number file to get.
+#'
+#' @examples
+#'  # Download the first homework file
+#'  \dontrun{download_homework(1)}
+#'
+#' @export
+
+download_homework <- function(homework_number){
+
+  base_path <- paste0(get_base_url(), "homework/")
+  file_name <- paste0("homework_", sprintf("%02d", homework_number), ".Rmd")
+
+  # if a file already exists with this name, don't download the file
+  #  but instead give an error message
+  check_file_exists(file_name)
+
+  full_path <- paste0(base_path, file_name)
+  download.file(full_path, file_name)
+
+}
+
+
+
+
+#' Download code related to the class
+#'
+#' This function downloads code that will be used in class from the class GitHub
+#' repository.
+#'
+#' @param class_number A number indicating which class we should get the code
+#'   for.
+#'
+#' @examples
+#'  # Download code from the first class
+#'  \dontrun{download_class_code(1)}
+#'
+#' @export
+download_class_code <- function(class_number) {
+
+  base_path <- paste0(get_base_url(), "class_code/")
+
+  result = tryCatch({
+
+    file_name <- paste0("class_", sprintf("%02d", class_number), ".Rmd")
+    full_path <- paste0(base_path, file_name)
+
+  },  warning = function(w) {
+
+    file.remove(file_name)
+    file_name <- paste0("class_", sprintf("%02d", class_number), ".R")
+    full_path <- paste0(base_path, file_name)
+
+  }, finally = {
+  })
+
+
+  # if a file already exists with this name, don't download the file
+  #  but instead give an error message
+  check_file_exists(file_name)
+
+  # if the file does not already exist, download it
+  download.file(full_path, file_name)
+
+}
+
+
+
+#' Download data related to the class
+#'
+#' This function downloads data related to the class from the class GitHub
+#' repository.
+#'
+#' @param file_name The name the file that has the data.
+#'
+#' @examples
+#'  # Download code from the first class
+#'  \dontrun{download_data("test_data.rda")}
+#'
+#' @export
+download_data <- function(file_name) {
+
+  base_path <- paste0(get_base_url(), "data/")
+  full_path <- paste0(base_path, file_name)
+
+  # if a file already exists with this name, don't download the file
+  #  but instead give an error message
+  check_file_exists(file_name)
+
+  # if the file does not already exist, download it
+  download.file(full_path, file_name)
+
+}
+
+
+
+
+
+
+#' Download an image related to the class
+#'
+#' This function downloads and image related to the class from the class GitHub
+#' repository.
+#'
+#' @param file_name The name the image to download.
+#'
+#' @examples
+#'  # Download an image from the class GitHub repository
+#'  \dontrun{download_image("valentin.png")}
+#'
+#' @export
+download_image <- function(file_name, force_download = FALSE){
+
+  base_path <- paste0(get_base_url(), "images/")
+  full_path <- paste0(base_path, file_name)
+
+  # only download the image if it doesn't exist or if force_download is TRUE
+  if (!file.exists(file_name) || force_download == TRUE) {
+    download.file(full_path, file_name)
+  }
+
+}
+
+
+
+
+
+
+### Helper functions -----------------------------------------------------
+
+
+# A helper function that checks if a file that is being downloaded already
+# exists. If the file exists and error message is given and the code is stopped.
+
+check_file_exists <- function(file_name) {
+
+  if (file.exists(file_name)){
+    stop(paste("The class code file you are trying to download", file_name,
+               "already exists. Please rename the file", file_name,
+               "and then rerun this function to download a new copy"))
+  }
+
+}
+
+
--- tests/testthat/test-download_functions.R
+++ tests/testthat/test-download_functions.R
@@ -1,3 +1,58 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+
+test_that("can download an image", {
+
+  download_image_name <- "valentin.png"
+  download_image(download_image_name)
+
+  expect_true(file.exists(download_image_name))
+
+  file.remove(download_image_name)   # clean up
+
 })
+
+
+
+test_that("can download data", {
+
+  download_data_name <- "test_data.rda"
+  download_data(download_data_name)
+
+  expect_true(file.exists(download_data_name))
+
+  file.remove(download_data_name)   # clean up
+
+})
+
+
+
+
+
+test_that("can download homework", {
+
+  download_homework(-1)
+  expected_file_name <- "homework_-1.Rmd"
+
+  expect_true(file.exists(expected_file_name))
+
+  file.remove(expected_file_name)   # clean up
+
+})
+
+
+
+
+
+test_that("can download class code", {
+
+  download_class_code(-1)
+  expected_file_name <- "class_-1.Rmd"
+
+  expect_true(file.exists(expected_file_name))
+
+  file.remove(expected_file_name)   # clean up
+
+})
+
+
+
