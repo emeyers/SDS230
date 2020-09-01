d397112edafdb68b1d2349769072cf1ff8f9a672
emeyers <emeyers@hampshire.edu>
2020-09-01 15:55:37 -0400

    Changed the function intial_setup to initial_setup(). Updated the package version number to reflect this.

--- R/initial_setup.R
+++ R/initial_setup.R
@@ -2,13 +2,12 @@
 
 # Code that can be run to intially set up a student's R installation.
 #  Here we are installing LaTeX through the tidytex package
-intial_setup <- function() {
+initial_setup <- function() {
 
   update_installed_packages()
   tinytex::install_tinytex()
 
 }
-
 
 
 
