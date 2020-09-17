7aa7de6705176166b0d6e133039bb4a10b3b5f67
emeyers <emeyers@hampshire.edu>
2020-09-17 12:02:08 -0400

    Updated the documentation for download_class_code to include the new argument for downloading the code with the solutions.

--- R/download_functions.R
+++ R/download_functions.R
@@ -40,6 +40,10 @@ download_homework <- function(homework_number) {
 #' @param class_number A number indicating which class we should get the code
 #'   for.
 #'
+#' @param with_answers A boolean indicating whether you want the class Markdown
+#'   document that contains all the code filled out, or you want the version
+#'   that will be used in class where we fill out the solutions as you go.
+#'
 #' @examples
 #'  # Download code from the first class
 #'  \dontrun{download_class_code(1)}
