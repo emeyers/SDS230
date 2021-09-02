4801f2f58f55053823ecbad9c298bf3552752f0d
emeyers <emeyers@hampshire.edu>
2021-09-02 18:05:47 -0400

    Updated class 1 slides and code

--- ClassMaterial/class_code/class_01.R
+++ ClassMaterial/class_code/class_01.R
@@ -1,5 +1,5 @@
 
-# 1.1 Basic arithmetic 
+# 1.1 Basic arithmetic
 2 + 2
 7 * 5
 
@@ -11,15 +11,15 @@ z  <- a + b
 z
 
 
-# 1.3 Number journey 
-a <- 7 
-b <- 52 
-x <- a * b 
+# 1.3 Number journey
+a <- 7
+b <- 52
+x <- a * b
 x
 
 
 # 2.1 Numeric, string and boolean data types
-a <- 7			
+a <- 7
 s <- "s is a terrible name for an object"
 b <- TRUE
 
@@ -28,8 +28,8 @@ class(s)
 
 
 
-# 3.1 Functions 
-sqrt(49)			
+# 3.1 Functions
+sqrt(49)
 tolower("DATA is AWESOME!")
 
 # 3.2 To get help information about a function or data set
@@ -46,12 +46,12 @@ sqrt(49)    # this takes the square root of 49
 # 5.1 Vectors are ordered sequences of numbers or letters
 # The c() function is used to create vectors
 
-v  <-  c(5, 232, 5, 543)	
-s  <-  c("statistics", "data", "science", "fun")		
+v  <-  c(5, 232, 5, 543)
+s  <-  c("statistics", "data", "science", "fun")
 
 
 # 5.2 One can access elements of a vector using square brackets []
-s[4]         # what will the answer be?    
+s[4]         # what will the answer be?
 
 
 # 5.3 We can get multiple elements from a vector too
@@ -60,7 +60,7 @@ s[c(1, 2)]
 
 # 5.4 One can assign a sequence of numbers to a vector
 z <- 2:10
-z[3]	
+z[3]
 
 # 5.5 One can test which elements are greater than a value
 z > 3
@@ -73,24 +73,6 @@ names(v) <- c("first", "second", "third", "fourth")
 z <- 2:10
 sqrt(z)
 mean(z)
-
-
-
-
-# 6.1  Data frames contain structured data (likely won't get to this until class 2)
-#  install.packages("okcupiddata")
-library(okcupiddata)
-View(profiles)        # the View() function only works in R Studio!
-
-
-# 6.2 We can make it visible using the data() function.
-data(profiles)
-
-# 6.3 We can extract the columns of a data frame as vector objects using the $ symbol
-the_ages <- profiles$age
-
-# 6.4 Can you get the mean() age of users
-mean(the_ages)
 
 
 
