6c1be6ccc0c93271a27b8245c7a22460e5130625
emeyers <emeyers@hampshire.edu>
2022-08-31 16:15:35 -0400

    Added class 1 code with and without answers.

---
+++ ClassMaterial/class_code/class_01.R
@@ -0,0 +1,79 @@
+
+# 1.1 Basic arithmetic 
+
+
+
+
+
+# 1.2 Assigning values to objects
+
+
+
+
+
+# 1.3 Number journey 
+
+
+
+
+
+# 2.1 Numeric, string and boolean data types
+
+
+
+
+
+
+
+
+# 3.1 Functions 
+
+
+
+# 3.2 To get help information about a function or data set
+
+
+
+# 4.1 Comments
+# One can add comments to your code to describe what you are doing
+
+
+
+
+# 5.1 Vectors are ordered sequences of numbers or letters
+# The c() function is used to create vectors
+
+
+	
+# 5.2 One can access elements of a vector using square brackets []
+
+
+
+# 5.3 We can get multiple elements from a vector too
+
+
+
+# 5.4 One can assign a sequence of numbers to a vector
+
+
+
+
+# 5.5 One can test which elements are greater than a value
+
+
+
+# 5.6 Can add names to vector elements
+
+
+
+
+# 5.7 One can also apply functions to vectors
+
+
+
+
+
+
+
+
+
